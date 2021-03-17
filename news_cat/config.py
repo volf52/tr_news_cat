92fdede2b31300f407deff71da981ac2e10d9502
Arslan <rslnkrmt2552@gmail.com>
2021-03-17 17:35:30 +0300

    Add basic app config (shared)
    - Pydantic model was used to enforce runtime validation, and using `BaseSettings` allows us to pass
    config using env variables etc.
    - `lru_cache` combined with `get_app_settings` ensures there's only one instance of config being shared
    throughout the application. There are other ways to accomplish this (using classmethods), but although
     they add more features, it comes with additional complexity.
    - `update_base` was used to update the base_dir and the fields that depend on it. The other way would
    be to set the dependent fields (metrics_dir, artifacts_dir etc) as properties, but that would not
    allow us to decouple those fields from `base_dir`. For instance, we may decide to set `data_dir` outside
    the project directory. Also, it would require additional code to add validation for those properties
    (using field aliases).

---
+++ news_cat/config.py
@@ -0,0 +1,35 @@
+from functools import lru_cache
+from pathlib import Path
+
+from pydantic import BaseSettings, DirectoryPath
+
+
+class AppSettings(BaseSettings):
+    base_dir: DirectoryPath = Path(__file__).parent.parent
+    data_dir: DirectoryPath = base_dir.joinpath("data")
+    artifact_dir: DirectoryPath = base_dir.joinpath("artifacts")
+    metrics_dir: DirectoryPath = base_dir.joinpath("metrics")
+
+    def update_base(self, new_base_pth: DirectoryPath) -> None:
+        """
+        Update base_dir and the fields that depend on it (data_dir, artifact_dir, metrics_dir)
+
+        Args:
+            new_base_pth: The new base directory path.
+
+        Returns: None
+
+        """
+
+        self.base_dir = new_base_pth
+        self.data_dir = self.base_dir.joinpath("data")
+        self.artifact_dir = self.base_dir.joinpath("artifacts")
+        self.metrics_dir = self.base_dir.joinpath("metrics")
+
+    class Config:
+        validate_assignment = True
+
+
+@lru_cache(maxsize=1)
+def get_app_settings() -> AppSettings:
+    return AppSettings()
