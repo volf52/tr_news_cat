5972fe96ad3cc5c1633f08bef8de9eba0f3fe135
Arslan <rslnkrmt2552@gmail.com>
2021-03-18 17:44:19 +0300

    Tfidf vectorizer with 25k features

--- news_cat/ml/config.py
+++ news_cat/ml/config.py
@@ -2,9 +2,25 @@ from dataclasses import dataclass
 
 
 @dataclass
+class Embedding:
+    tfidf_vectorizer = "tfidf.jlib"
+
+    train_tfidf = "train_tfidf.npy"
+    test_tfidf = "test_tfidf.npy"
+    valid_tfidf = "valid_tfidf.npy"
+
+    trainY = "train_lbl.npy"
+    testY = "test_lbl.npy"
+    validY = "valid_lbl.npy"
+
+
+@dataclass
 class MLConfig:
     raw_data_input = "raw_data.csv"
     raw_data_df_out = "raw_data.feather"
+
     train_df = "train_df.feather"
     test_df = "test_df.feather"
     valid_df = "valid_df.feather"
+
+    embedding = Embedding()
